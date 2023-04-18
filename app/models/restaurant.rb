2710e28468d4c897ef6fbea5f322ed0705ca8032
katya sitko <kat.sitko@gmail.com>
2023-04-18 21:25:18 +0200

    added validations

---
+++ app/models/restaurant.rb
@@ -0,0 +1,13 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
+end
+
+
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: 0..5 }
+end
