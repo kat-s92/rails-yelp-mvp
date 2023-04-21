fed56de868a4cc288966efe897173ccd82b18939
katya sitko <kat.sitko@gmail.com>
2023-04-21 20:57:08 +0200

    Added New Resto page and styles

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,23 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @review = Review.new
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = @Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
