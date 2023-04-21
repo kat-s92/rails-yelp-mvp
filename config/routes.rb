8314c26870f9ae578a3f2c99af0e3ce393b89e0b
katya sitko <kat.sitko@gmail.com>
2023-04-21 19:58:56 +0200

    Added Review content

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: %i[index new create show] do
-    resources :reviews, only: %i[show new create]
+    resources :reviews, only: %i[new create]
   end
+  resources :reviews, only: [:destroy]
 end
